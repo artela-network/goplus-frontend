6f57845c5f7a9d7c7738bdf4a190aa4b4a1a9838
dumbeng <jack@artela.network>
2024-03-29 14:43:39 +0800

    Stop auto refresh if all task done

--- src/layout/activity/index.tsx
+++ src/layout/activity/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import './activity.css'
 import Introduce from '../components/introduce/index'
@@ -14,43 +14,75 @@ export default function Activity() {
   const { account } = useActiveWeb3React()
   const [taskStatus, setTaskStatus] = useState(0)
   const [taskInfos, setTaskInfos] = useState([])
-  const location = useLocation();
+  const location = useLocation()
+  const intervalId = useRef<NodeJS.Timeout | null>(null)
+
   const getQueryParams = () => {
-    const queryParams = new URLSearchParams(location.search);
-    const taskId = queryParams.get('taskId');
+    const queryParams = new URLSearchParams(location.search)
+    const taskId = queryParams.get('taskId')
     if (taskId !== null) {
       return taskId.split('/')[0]
     } else {
       return ''
     }
   }
-  const getTaskList = async (attempt = 0) => {
+  const getTaskList = async (attempt = 0): Promise<boolean> => {
     if (account) {
       const res = await getTaskListByAccount(account)
       if (res.success) {
         setTaskStatus(res.data.status)
         if (res.data.taskInfos) {
           setTaskInfos(res.data.taskInfos)
+
+          for (let i = 0; i < res.data.taskInfos.length; i++) {
+            if (res.data.taskInfos[i].taskStatus !== 3) {
+              return false
+            }
+          }
+
+          return true
         } else if (attempt < 2) {
           const initRes = await initTaskListByAccount(account, getQueryParams())
           if (initRes.success) {
-            await getTaskList(attempt + 1)
+            return getTaskList(attempt + 1)
           }
         }
       }
     } else {
       setTaskInfos([])
     }
+
+    return false
   }
 
   useEffect(() => {
-    getTaskList()
+    if (!account) {
+      return
+    }
 
-    const interval = setInterval(async () => {
-      await getTaskList()
-    }, 8000)
+    const init = async () => {
+      let allDone = await getTaskList()
 
-    return () => clearInterval(interval)
+      if (intervalId.current) {
+        clearInterval(intervalId.current)
+      }
+
+      if (!allDone) {
+        intervalId.current = setInterval(async () => {
+          allDone = await getTaskList()
+          if (allDone && intervalId.current) {
+            clearInterval(intervalId.current)
+            intervalId.current = null
+          }
+        }, 8000)
+      }
+    }
+
+    init()
+
+    return () => {
+      if (intervalId.current) clearInterval(intervalId.current)
+    }
   }, [account])
 
   return (
@@ -59,7 +91,7 @@ export default function Activity() {
       <TaskList />
       <FirstTask taskInfo={taskInfos[1]} />
       <SecondTask taskInfo={taskInfos[2]} />
-      <ThirdTask getTaskList={getTaskList}  taskInfo={taskInfos[3]} />
+      <ThirdTask getTaskList={getTaskList} taskInfo={taskInfos[3]} />
       <Explain />
     </div>
   )
